bbb18864e30295dc5245ca1f0c292ece113e2c21
xDamneDx <xdamnedx@gmail.com>
2022-05-27 19:20:15 +0300

    Created useLogout custom hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,17 @@
+// Firebase imports:
+import { auth } from "../firebase/config";
+import { signOut } from "firebase/auth";
+
+export const useLogout = () => {
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  return { logout };
+};
