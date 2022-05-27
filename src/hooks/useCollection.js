35ed125c07d625a846bbc26e799b420aa1d5380d
xDamneDx <xdamnedx@gmail.com>
2022-05-27 14:54:05 +0300

    Created useCollection custom hook

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase/config";
+
+// Firebase imports:
+import { collection, onSnapshot } from "firebase/firestore";
+
+export const useCollection = (c) => {
+  const [documents, setDocuments] = useState(null);
+
+  useEffect(() => {
+    let ref = collection(db, c);
+
+    const unsub = onSnapshot(ref, (snapshot) => {
+      let results = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() });
+      });
+      setDocuments(results);
+    });
+
+    return () => unsub();
+  }, [c]);
+
+  return { documents };
+};
