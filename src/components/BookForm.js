4022a6b309ce6e1929f65717f35b50f800cd2ea6
xDamneDx <xdamnedx@gmail.com>
2022-05-30 11:32:50 +0300

    Added uid to the book object

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // Firebase imports:
 import { db } from "../firebase/config";
@@ -6,11 +7,12 @@ import { collection, addDoc } from "firebase/firestore";
 
 export default function BookForm() {
   const [newBook, setNewBook] = useState("");
+  const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await addDoc(collection(db, "books"), { title: newBook });
+    await addDoc(collection(db, "books"), { title: newBook, uid: user.uid });
 
     setNewBook("");
   };
